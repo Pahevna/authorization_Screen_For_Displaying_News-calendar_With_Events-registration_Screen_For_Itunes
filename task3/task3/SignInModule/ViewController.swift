4af6e40674107ae3d557bad48e534aaed0386966
Pahevna <afpavel@inbox.ru>
2020-10-27 22:30:57 +0400

    add gradient

--- task3/task3/SignInModule/ViewController.swift
+++ task3/task3/SignInModule/ViewController.swift
@@ -11,20 +11,19 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var logInView: UIView!
-    @IBOutlet weak var txtUserName: UITextField!
-    @IBOutlet weak var txtPassword: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.setGradientBackround(colorOne: .blue, colorTwo: .cyan)
+        view.setGradientBackround(colorOne: UIColor(red: 75.0/255.0, green: 92.0/255.0, blue: 202.0/255.0,                                               alpha: 1.0),
+                                  colorTwo: UIColor(red: 0.0/255.0, green: 152.0/255.0, blue: 209.0/255.0,                     alpha: 1.0))
         
         logInButton.layer.cornerRadius = logInButton.frame.size.height/2
         logInButton.layer.masksToBounds = true
         
         logInView.layer.cornerRadius = 10
         logInView.clipsToBounds = true
-        
+
     }
 }
 
